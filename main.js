c7146c0919c112fbf12309f80bba773d9faa3970
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2022-06-21 12:44:36 +0700

    change Jun 21, 2022

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const $content = document.getElementById('content');
 const $button  = document.querySelector('.button');
+
+$button.addEventListener('click', ()=> {
+    $content.classList.toggle('active');
+    $content.textContent = 'for instance like this!';
+    if($content.classList.contains('active')){
+        $content.textContent = 'any text may be here...';
+    }
+
+
+});
